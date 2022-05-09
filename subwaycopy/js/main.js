41647805aa6fe9d390ef2c16b7b73a1d0b4ec699
전인하 <104603885+JeonInha@users.noreply.github.com>
2022-05-09 17:43:09 +0900

    Add files via upload

--- subwaycopy/js/main.js
+++ subwaycopy/js/main.js
@@ -2,7 +2,7 @@ $(function () {
     $('.main_slider').slick({
         arrows: false,
         autoplay: true,
-        autoplaySpeed: 1000,
+        autoplaySpeed: 3000,
         // 1000 = 1s / 자바스크립트에서 시간을 쓸 땐 ms 단위로 씀
         dots: true
     })
